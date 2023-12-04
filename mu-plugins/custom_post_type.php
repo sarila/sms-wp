999b845bbae04b29ea4bd26c9f083b861d40d8dc
Sarila <ngakhusisarila@gmail.com>
2023-12-05 00:02:45 +0545

    dynamic content update for about us

--- mu-plugins/custom_post_type.php
+++ mu-plugins/custom_post_type.php
@@ -1,7 +1,11 @@
 <?php
 
 add_action('init', 'sms_team_post_type', 0);
+add_action('init', 'sms_sponsors_post_type', 0);
+add_action('init', 'sms_beneficiaries_post_type', 0);
 add_action('init', 'sms_budget_data_post_type', 0);
+add_action('init', 'sms_how_work_post_type', 0);
+add_action('init', 'sms_request_status_post_type', 0);
 
 //custom post type for FAQ
 function sms_team_post_type()
@@ -44,8 +48,6 @@ function sms_team_post_type()
     // register the post Type
     register_post_type('team', $args);
 }
-
-
 function sms_budget_data_post_type()
 {
     // Labels for the Post Type
@@ -69,7 +71,7 @@ function sms_budget_data_post_type()
         'label' => __('Budget Data', 'sms'),
         'description' => __('Budget Data of Support My School', 'sms'),
         'labels' => $labels,
-        'supports' => array('title', 'thumbnail', 'excerpt', 'comments', 'author'),
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt', 'comments', 'author'),
         'hierarchical' => false,
         'public' => true,
         'show_ui' => true,
@@ -87,6 +89,171 @@ function sms_budget_data_post_type()
     register_post_type('budget_data', $args);
 }
 
+
+function sms_how_work_post_type()
+{
+    // Labels for the Post Type
+    $labels = array(
+        'name' => _x('How it Works', 'Post Type General Name', 'sms'),
+        'singular_name' => _x('How it Works', 'Post Type Singular Name', 'sms'),
+        'menu_name' => __('How it Works', 'sms'),
+        'parent_item_colon' => __('Parent How it Works', 'sms'),
+        'all_items' => __('All How it Works', 'sms'),
+        'view_item' => __('View How it Works', 'sms'),
+        'add_new_item' => __('Add New How it Works', 'sms'),
+        'add_new' => __('Add New How it Works', 'sms'),
+        'edit_item' => __('Edit How it Works', 'sms'),
+        'update_item' => __('Update How it Works', 'sms'),
+        'search_items' => __('Search How it Works', 'sms'),
+        'not_found' => __('No How it Works found', 'sms'),
+        'not_found_in_trash' => __('Not found in trash', 'sms'),
+    );
+    // Another Customizations
+    $args = array(
+        'label' => __('How it Works', 'sms'),
+        'description' => __('How it Works of Support My School', 'sms'),
+        'labels' => $labels,
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menus' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 14,
+        'menu_icon' => 'dashicons-editor-help',
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'page',
+    );
+    // register the post Type
+    register_post_type('how_it_works', $args);
+}
+
+function sms_beneficiaries_post_type()
+{
+    // Labels for the Post Type
+    $labels = array(
+        'name' => _x('Beneficiaries', 'Post Type General Name', 'sms'),
+        'singular_name' => _x('Beneficiary', 'Post Type Singular Name', 'sms'),
+        'menu_name' => __('Beneficiaries', 'sms'),
+        'parent_item_colon' => __('Parent Beneficiary', 'sms'),
+        'all_items' => __('All Beneficiaries', 'sms'),
+        'view_item' => __('View Beneficiary', 'sms'),
+        'add_new_item' => __('Add New Beneficiary', 'sms'),
+        'add_new' => __('Add New Beneficiary', 'sms'),
+        'edit_item' => __('Edit Beneficiary', 'sms'),
+        'update_item' => __('Update Beneficiary', 'sms'),
+        'search_items' => __('Search Beneficiary', 'sms'),
+        'not_found' => __('No Beneficiaries found', 'sms'),
+        'not_found_in_trash' => __('Not found in trash', 'sms'),
+    );
+    // Another Customizations
+    $args = array(
+        'label' => __('Beneficiaries', 'sms'),
+        'description' => __('Beneficiaries of Support My School', 'sms'),
+        'labels' => $labels,
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menus' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 15,
+        'menu_icon' => 'dashicons-buddicons-groups',
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'page',
+    );
+    // register the post Type
+    register_post_type('beneficiaries', $args);
+}
+
+
+function sms_sponsors_post_type()
+{
+    // Labels for the Post Type
+    $labels = array(
+        'name' => _x('Supporters', 'Post Type General Name', 'sms'),
+        'singular_name' => _x('Supporters', 'Post Type Singular Name', 'sms'),
+        'menu_name' => __('Supporters', 'sms'),
+        'parent_item_colon' => __('Parent Supporters', 'sms'),
+        'all_items' => __('All Supporters', 'sms'),
+        'view_item' => __('View Supporters', 'sms'),
+        'add_new_item' => __('Add New Supporters', 'sms'),
+        'add_new' => __('Add New Supporters', 'sms'),
+        'edit_item' => __('Edit Supporters', 'sms'),
+        'update_item' => __('Update Supporters', 'sms'),
+        'search_items' => __('Search Supporters', 'sms'),
+        'not_found' => __('No Supporters found', 'sms'),
+        'not_found_in_trash' => __('Not found in trash', 'sms'),
+    );
+    // Another Customizations
+    $args = array(
+        'label' => __('Supporters', 'sms'),
+        'description' => __('Supporters of Support My School', 'sms'),
+        'labels' => $labels,
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menus' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 15,
+        'menu_icon' => 'dashicons-networking',
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'page',
+    );
+    // register the post Type
+    register_post_type('supporters', $args);
+}
+
+function sms_request_status_post_type()
+{
+    // Labels for the Post Type
+    $labels = array(
+        'name' => _x('Request Status', 'Post Type General Name', 'sms'),
+        'singular_name' => _x('Request Status', 'Post Type Singular Name', 'sms'),
+        'menu_name' => __('Request Status', 'sms'),
+        'parent_item_colon' => __('Parent Request Status', 'sms'),
+        'all_items' => __('All Request Status', 'sms'),
+        'view_item' => __('View Request Status', 'sms'),
+        'add_new_item' => __('Add New Request Status', 'sms'),
+        'add_new' => __('Add New Request Status', 'sms'),
+        'edit_item' => __('Edit Request Status', 'sms'),
+        'update_item' => __('Update Request Status', 'sms'),
+        'search_items' => __('Search Request Status', 'sms'),
+        'not_found' => __('No Request Status found', 'sms'),
+        'not_found_in_trash' => __('Not found in trash', 'sms'),
+    );
+    // Another Customizations
+    $args = array(
+        'label' => __('Request Status', 'sms'),
+        'description' => __('Request Status of Support My School', 'sms'),
+        'labels' => $labels,
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menus' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 15,
+        'menu_icon' => 'dashicons-format-aside',
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'page',
+    );
+    // register the post Type
+    register_post_type('request_status', $args);
+}
 
 
 
--- themes/sms/page-about.php
+++ themes/sms/page-about.php
@@ -3,189 +3,168 @@
 <?php get_header(); ?>
 <?php get_template_part('innerheader-special') ?>
 
-<!-- Service Start -->
-<div class="container-xxl py-5">
-  <div class="container">
-    <div class="row g-4">
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="service-item text-center pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
-            <h5 class="mb-3">Skilled Instructors</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="service-item text-center pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-globe text-primary mb-4"></i>
-            <h5 class="mb-3">Online Classes</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-        <div class="service-item text-center pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-home text-primary mb-4"></i>
-            <h5 class="mb-3">Home Projects</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-        <div class="service-item text-center pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
-            <h5 class="mb-3">Book Library</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Service End -->
+<?php while (have_posts()):
+  the_post(); ?>
 
 
 <!-- About Start -->
 <div class="container-xxl py-5">
-  <div class="container">
-    <div class="row g-5">
-      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
-        <div class="position-relative h-100">
-          <img class="img-fluid position-absolute w-100 h-100" src="img/about.jpg" alt="" style="object-fit: cover;">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
+                <div class="position-relative h-100">
+                    <img class="img-fluid position-absolute w-100 h-100" src="<?php the_post_thumbnail_url(); ?>" alt=""
+                        style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
+                <?php
+                  the_content();
+                  ?>
+
+                <!-- <div class="row gy-2 gx-4 mb-4">
+                  <div class="col-sm-6">
+                    <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                  </div>
+                  <div class="col-sm-6">
+                    <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
+                  </div>
+                  <div class="col-sm-6">
+                    <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
+                  </div>
+                  <div class="col-sm-6">
+                    <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                  </div>
+                  <div class="col-sm-6">
+                    <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
+                  </div>
+                  <div class="col-sm-6">
+                    <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
+                  </div>
+                    </div> -->
+                <a class="btn btn-primary py-3 px-5 mt-2" href="<?php echo site_url('/contact') ?>">Read More</a>
+            </div>
         </div>
-      </div>
-      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-        <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
-        <h1 class="mb-4">Welcome to eLEARNING</h1>
-        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
-          eos. Clita erat ipsum et lorem et sit.</p>
-        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
-          eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-        <div class="row gy-2 gx-4 mb-4">
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
-          </div>
-        </div>
-        <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
-      </div>
     </div>
-  </div>
 </div>
 <!-- About End -->
 
 
 <!-- Team Start -->
 <div class="container-xxl py-5">
-  <div class="container">
-    <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-      <h6 class="section-title bg-white text-center text-primary px-3">Instructors</h6>
-      <h1 class="mb-5">Expert Instructors</h1>
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title bg-white text-center text-primary px-3">Our Team</h6>
+            <h1 class="mb-5">Founders</h1>
+        </div>
+        <div class="row g-3">
+            <?php
+            $args = array(
+              'post_type' => 'team',
+              'orderby' => 'title',
+              'order' => 'ASC',
+            );
+            $teams = new WP_Query($args);
+            while ($teams->have_posts()):
+              $teams->the_post();
+              ?>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="team-item bg-light">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="<?php echo the_post_thumbnail_url() ?>" alt="<?php the_title();
+                               the_field('designation'); ?>">
+                    </div>
+                    <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
+                        <div class="bg-light d-flex justify-content-center pt-2 px-1">
+                            <?php if (get_field('linkedin_link')): ?>
+                            <a class="btn btn-sm-square btn-primary mx-1" href="<?php the_field('linkedin_link'); ?>"
+                                target="_blank"><i class="fab fa-linkedin"></i></a>
+                            <?php endif; ?>
+                            <?php if (get_field('facebook_link')): ?>
+
+                            <a class="btn btn-sm-square btn-primary mx-1" href="<?php the_field('facebook_link'); ?>"
+                                target="_blank"><i class="fab fa-facebook"></i></a>
+                            <?php endif; ?>
+
+                            <?php if (get_field('instagram_link')): ?>
+
+                            <a class="btn btn-sm-square btn-primary mx-1" href="<?php the_field('instagram_link'); ?>"
+                                target="_blank"><i class="fab fa-instagram"></i></a>
+                            <?php endif; ?>
+
+                            <?php if (get_field('twitter_link')): ?>
+
+                            <a class="btn btn-sm-square btn-primary mx-1" href="<?php the_field('twitter_link'); ?>"
+                                target="_blank"><i class="fab fa-twitter"></i></a>
+                            <?php endif; ?>
+
+                        </div>
+                    </div>
+                    <div class="text-center p-4">
+                        <h5 class="mb-0"><?php the_title(); ?>
+                        </h5>
+                        <small>
+                            <?php the_field('designation'); ?>
+                        </small>
+                    </div>
+                </div>
+            </div>
+            <?php endwhile;
+              wp_reset_postdata(); ?>
+
+        </div>
     </div>
-    <div class="row g-4">
-      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="team-item bg-light">
-          <div class="overflow-hidden">
-            <img class="img-fluid" src="img/team-1.jpg" alt="">
-          </div>
-          <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-          <div class="text-center p-4">
-            <h5 class="mb-0">Instructor Name</h5>
-            <small>Designation</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="team-item bg-light">
-          <div class="overflow-hidden">
-            <img class="img-fluid" src="img/team-2.jpg" alt="">
-          </div>
-          <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-          <div class="text-center p-4">
-            <h5 class="mb-0">Instructor Name</h5>
-            <small>Designation</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-        <div class="team-item bg-light">
-          <div class="overflow-hidden">
-            <img class="img-fluid" src="img/team-3.jpg" alt="">
-          </div>
-          <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-          <div class="text-center p-4">
-            <h5 class="mb-0">Instructor Name</h5>
-            <small>Designation</small>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-        <div class="team-item bg-light">
-          <div class="overflow-hidden">
-            <img class="img-fluid" src="img/team-4.jpg" alt="">
-          </div>
-          <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-              <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-          <div class="text-center p-4">
-            <h5 class="mb-0">Instructor Name</h5>
-            <small>Designation</small>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 <!-- Team End -->
 
-
-<main>
-  <section class="section-contact-us">
+<!-- Our Supporters Start -->
+<div class="container-xxl py-5">
     <div class="container">
-      <h3 class="section-contact-us_title">Contact US</h3>
-      <div class="row row-cols-12 row-cols-md-1 row-cols-lg-2 section-contact-us_row">
-        <div class="col col-lg-8 g-0">
-          <div class="row g-3 section-contact-us_form">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title bg-white text-center text-primary px-3">Our Supporters</h6>
+            <h1 class="mb-5"></h1>
+        </div>
+        <div class="row g-4">
+            <?php
+            $args = array(
+              'post_type' => 'supporters',
+              'orderby' => 'title',
+              'order' => 'ASC',
+            );
+            $supporters = new WP_Query($args);
+            while ($supporters->have_posts()):
+              $supporters->the_post();
+              ?>
+
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item text-center pt-3">
+                    <div class="p-4">
+                        <img class="img-fluid" src="<?php echo the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>">
+                        <h5 class="mb-3">
+                            <?php the_title(); ?>
+                        </h5>
+                        <p>
+                            <!-- <?php the_excerpt(); ?> -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <?php endwhile;
+              wp_reset_postdata(); ?>
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+
+<!-- <main>
+    <section class="section-contact-us">
+      <div class="container">
+        <h3 class="section-contact-us_title">Contact US</h3>
+        <div class="row row-cols-12 row-cols-md-1 row-cols-lg-2 section-contact-us_row">
+          <div class="col col-lg-8 g-0">
+            <div class="row g-3 section-contact-us_form">
             <?php
             // echo do_shortcode('[contact-form-7 id="25" title="Contact Us"]');
             ?>
@@ -254,5 +233,7 @@
       </div>
     </div>
   </section>
-</main>
+</main> -->
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
