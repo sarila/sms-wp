d1045221005ef7c33feee824d13b866eafb134bc
devhimal <himalhobbs.364@gmail.com>
2023-12-12 16:50:29 +0545

    feat: added hero section and logo in navbar.

--- themes/sms/front-page.php
+++ themes/sms/front-page.php
@@ -1,55 +1,29 @@
 <?php get_header(); ?>
 
 
+<!-- start first hero section -->
 
-
-<!-- About Start -->
-<!-- <div class="container-xxl py-5">
-  <div class="container">
-    <div class="row g-5">
-      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
-        <div class="position-relative h-100">
-          <img class="img-fluid position-absolute w-100 h-100"
-            src="<?php echo get_template_directory_uri() ?>/includes/assets/img/about.jpg" alt=""
-            style="object-fit: cover;">
-        </div>
-      </div>
-      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-        <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
-        <h1 class="mb-4">Welcome to eLEARNING</h1>
-        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
-          eos. Clita erat ipsum et lorem et sit.</p>
-        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
-          eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
-        </p>
-        <div class="row gy-2 gx-4 mb-4">
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate
-            </p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
-          </div>
-          <div class="col-sm-6">
-            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate
-            </p>
-          </div>
-        </div>
-        <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
-      </div>
+<div class="hero_container">
+  <div class="hero_image_wrapper">
+    <img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/includes/assets/img/governmentSchool.jpeg" alt="student_image">
+  </div>
+  <div class="hero_button_wrapper">
+    <div>
+      <h1>For every student,
+        every teacher.
+        every parent.</h1>
+      <p>We're a nonprofit, and we want to give you reports about school budget. If you're not satisfied and think your school isn't doing what the government says it should for public schools, we're here to help.<br /> <b>Report through following links:</b></p>
+    </div>
+    <div class="button_wrapper">
+      <button>Students</button>
+      <button>Teachers</button>
+      <button>Parents</button>
+      <button>Others</button>
     </div>
   </div>
-</div> -->
-<!-- About End -->
+</div>
+
+<!-- end of first hero section -->
 
 
 <!-- hero section start -->
@@ -322,8 +296,8 @@
         <a class="position-relative d-block h-100 overflow-hidden" href="">
           <img class="img-fluid position-absolute w-100 h-100" src="<?php echo get_template_directory_uri() ?>/includes/assets/img/public.jpg" alt="" style="height: auto; width: 100%; object-fit: cover" style="object-fit: cover;">
           <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin:  1px;">
-            <h5 class="m-0">Public</h5>
             <small class="text-primary">2 Millions +</small>
+            <h5 class="m-0">Public</h5>
           </div>
         </a>
       </div>
@@ -331,7 +305,6 @@
   </div>
 </div>
 <!-- Categories Start -->
-
 
 <!-- blog section start -->
 <div class="blog_container" id="blog">
